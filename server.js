9838afb2a057c821588dfb134e7cafff43050ef3
Zach Zager <zachzager@gmail.com>
2016-10-03 14:58:54 -0400

    adding files

---
+++ server.js
@@ -0,0 +1,90 @@
+// server.js
+
+// Express initialization
+var express = require('express');
+var bodyParser = require('body-parser');
+var validator = require('validator');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.all('/*', function(request, response, next) {
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+// Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/FOOD_EVENTS_V1';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+	var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+});
+
+// get data from app
+app.post('/post', function (request, response) {
+
+	// VALIDATE INPUT
+
+
+	//console.log(request);
+	console.log(request.body);
+
+	db.collection('events_list', function(err, collection) {	
+		collection.insert( {"Date":request.body.date, "Time":request.body.time, 
+				"Food":request.body.food, "Sponsor":request.body.sponsor,
+				"Location":request.body.location, "Other":request.body.other} );
+	});
+
+
+	response.send();
+
+});
+
+// get facebook data
+// (include parsing)
+
+// REACH: get email data
+// (include parsing)
+
+// get all data
+app.get('/', function (request, response) {
+	response.set('Content-Type', 'text/html');
+
+	db.collection('events_list', function(err, collection) {
+		
+		//collection.find({"Food": request.query.Food}).toArray(function(err, cursor) {
+			collection.find().toArray(function(err, cursor) {
+			if (!err) {
+				console.log(cursor);
+				response.send(cursor);
+			} else {
+				console.log('bad');
+				response.send([]);
+			}
+		});
+		
+	});
+
+});
+
+
+// Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
