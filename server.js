7ac03cd32ef9ff1cd4c3f312c04edd1dc7e17db7
Zach Zager <zachzager@gmail.com>
2016-10-03 17:24:26 -0400

    removed date checking functionality until further info on how to implement is found

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.all('/*', function(request, response, next) {
 });
 
 // Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
-//var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/FOOD_EVENTS_V1';
+// var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/FOOD_EVENTS_V1';
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://heroku_h5ng6mhk:8lbubrnjqrhck82sle6t0j1856@ds049446.mlab.com:49446/heroku_h5ng6mhk';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 	var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
@@ -29,23 +29,24 @@ app.post('/post', function (request, response) {
 	// VALIDATE INPUT
 
 	// get date of input
-	req_date = new Date(request.body.date);
-	req_date = req_date.setDate(req_date.getDate()+1);
+	//req_date = new Date(request.body.date);
+	// req_date = Date.parse(request.body.date);
+	// req_date = req_date.setDate(req_date.getDate()+1);
 
-	date = new Date();
+	// date = new Date();
 
 	// checks for proper parameters, if true insert input to database
 	// Checks that:
 	// - fields are strings
 	// - fields (except "sponsor" and "other") are not blank strings
-	// - day of event has not already passed
+	// - [UNDER CONSTRUCTION] day of event has not already passed
 	if ( (typeof request.body.date === "string" && typeof request.body.time === "string" 
 				&& typeof request.body.food === "string" && typeof request.body.sponsor === "string"
 				&& typeof request.body.location === "string" && typeof request.body.other === "string")
 		&& (request.body.date.length > 0 && request.body.time.length > 0 
-				&& request.body.food.length > 0 && request.body.location.length > 0)
-		&& (req_date.getMonth() > date.getMonth() || 
-				(req_date.getMonth() === date.getMonth() && req_date.getDate() >= date.getDate())) ) {
+				&& request.body.food.length > 0 && request.body.location.length > 0) ) {
+		// && (req_date.getMonth() > date.getMonth() || 
+		// 		(req_date.getMonth() === date.getMonth() && req_date.getDate() >= date.getDate())) ) {
 		
 		db.collection('events_list', function(err, collection) {	
 			collection.insert( {"Date":request.body.date, "Time":request.body.time, 
